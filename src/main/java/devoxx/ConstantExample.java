4c2035784d24c24adacc5143d5f112c643754dcb
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2024-04-16 10:50:59 +0200

    Add ConstantExample

---
+++ src/main/java/devoxx/ConstantExample.java
@@ -0,0 +1,11 @@
+package devoxx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstantExample {
+
+  private static final Map<String, Integer> decoratorsOrder = new HashMap<>();
+
+
+}
