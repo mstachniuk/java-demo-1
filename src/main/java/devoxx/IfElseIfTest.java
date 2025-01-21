5bf71cfed3e5ab5136aff8fdc7bdcaaa0d51ecc5
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2025-01-21 16:52:44 +0100

    Add If else if example 2

--- src/main/java/devoxx/IfElseIfTest.java
+++ src/main/java/devoxx/IfElseIfTest.java
@@ -1,9 +1,25 @@
 package devoxx;
 
 class IfElseIfTest {
-  if (true) {
-    System.out.println("x");
-  } else if (true) {
-    throw new RuntimeException("boom");
+  public static void main(String[] args) {
+    if (true) {
+      System.out.println("x");
+    } else if (true) {
+      throw new RuntimeException("boom");
+    }
+
+    if (true) {
+      System.out.println("x");
+    } else if (true) {
+      System.out.println("y");
+    }
+
+        if (true) {
+      System.out.println("x");
+    } else if (true) {
+      System.out.println("y");
+    } else {
+      System.out.println("z");
+    }
   }
 }
